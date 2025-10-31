48353bb34890d650ccf93683ccaf8db65f6fcf4b
poornimaj-star <PoornimaJ@first-insight.com>
2025-10-31 10:07:22 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import LiveTemplatesDemo from './components/LiveTemplatesDemo';
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <LiveTemplatesDemo />
+    </div>
+  );
+};
+
+export default App;
